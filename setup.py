0d445b080c678221819b8e35cdc3a7a8d38c3968
thorkill <thkr@hannover.ccc.de>
2015-04-15 17:44:00 +0200

    Added example setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+from setuptools import setup, find_packages
+setup(
+    name = "TincInfo",
+    version = "0.1",
+    packages = ['tinctools'],
+    package_dir = {'tinctools': './src/'}
+    )
