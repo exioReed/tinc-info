9f7c14c4252702b9d94b31d140597917fca01fdb
Maximilian Kaulmann <kaulmann@stud.uni-hannover.de>
2015-04-27 23:23:19 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "tinctools",
-    version = "0.2",
+    version = "0.3",
     packages = ['tinctools'],
     package_dir = {'tinctools': './src/'}
     )
